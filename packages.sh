f47eddd5bf0bae7f72f490cf3c3d1e80bdfec934
MTAC <mtactheming@gmail.com>
2017-09-11 16:54:54 -0400

    no message

--- packages.sh
+++ packages.sh
@@ -2,3 +2,7 @@
 dpkg-deb -bZgzip projects/i7walls debs
 dpkg-deb -bZgzip projects/SetGen debs
 dpkg-deb -bZgzip projects/Savethemblobs debs
+dpkg-deb -bZgzip projects/Jailbreak\ Springy debs
+dpkg-deb -bZgzip projects/phoenixfix debs
+dpkg-deb -bZgzip projects/Terminal\ Power debs
+dpkg-deb -bZgzip projects/Verbose\ Springy debs
